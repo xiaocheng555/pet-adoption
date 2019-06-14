de613bdc3d33919be5f31247f7d895a3d722afd7
xiaocheng555 <1052745517@qq.com>
2019-06-14 18:00:13 +0800

    feat 完成表单验证逻辑

--- config/validate.js
+++ config/validate.js
@@ -8,27 +8,42 @@
  * Modified By: zhanghc11 (huancheng.zhang@meicloud.com>)
  */
 
-//  全局的正则表达式
+//  全局的正则表达式或者验证函数
 const pattern = {
   // 邮箱
   mail: /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/,
   // 国际电话（5-15位数字）
-  telephone: /^\d{5,15}$/
+  telephone: /^\d{5,15}$/,
+	// 非空字符串、数组
+	nonEmpty: /\S+/
 }
 
 // 初始化表单校验规则
 function initValidateConfig (pattern) {
-  const validate = {}
+	// 存储所有验证规则
+  const _validateConfig = {}
+	
   Object.entries(pattern).forEach(([key, reg]) => {
-    validate[key] = function (rule, value, callback) {
-      if (reg.test(value)) {
-        callback()
-      } else {
-        callback(new Error())
-      }
-    }
+		let validateFn
+		if (isRegExp(reg)) {
+			// 将正则表达式转化为c-form组件（同el-element）可使用的验证函数
+			validateFn = function (rule, value, callback) {
+			  if (reg.test(value.toString())) {
+			    callback()
+			  } else {
+			    callback(new Error())
+			  }
+			}
+		} else {
+			validateFn = reg
+		}
+		_validateConfig[key] = (validateFn)
   })
-  return validate
+  return _validateConfig
+}
+
+function isRegExp (value) {
+	return Object.prototype.toString.call(value) === '[object RegExp]'
 }
 
 const validateConfig = initValidateConfig(pattern)
