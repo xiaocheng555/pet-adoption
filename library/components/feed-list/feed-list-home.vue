39e101eb2607b03900cf5dcd2ecb03e0b763bb56
xiaocheng555 <1052745517@qq.com>
2019-06-19 22:36:15 +0800

    feat 领养页面切图

---
+++ library/components/feed-list/feed-list-home.vue
@@ -0,0 +1,103 @@
+<template>
+	<view class="feed-list-home">
+		<view 
+			class="feed-list-item" 
+			v-for="(item, index) in list" 
+			:key="index"
+			@tap="handleItemClick(item, index)">
+			<view class="feed-list-header">
+				<image class="feed-list-avatar" :src="item.avatar"></image>
+				<text class="feed-list-name">{{ item.name }}</text>
+			</view>
+			<view class="feed-list-body">
+				<view class="feed-list-content c-ellipsisLn-6">{{ item.content }}</view>
+				<grid-images :list="item.petImages" :limit="3"></grid-images>
+				<view class="feed-list-tags">
+					<c-tag>未绝育</c-tag>
+					<c-tag>未绝育</c-tag>
+					<c-tag>未绝育</c-tag>
+				</view>
+				<view class="feed-list-address">
+					<image class="feed-list-location-icon" :src="locationIcon"></image>
+					{{ item.address }}
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import GridImages from '@/library/components/grid-images'
+	import locationIcon from '@/static/icons/location-grey.svg'
+	export default {
+		components: {
+			GridImages
+		},
+		props: {
+			list: {
+				type: Array,
+				default: () => []
+			}
+		},
+		data () {
+			return {
+				locationIcon
+			}
+		},
+		methods: {
+			handleItemClick (item, index) {
+				this.$emit('item-click', item, index)
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+.feed-list-home {
+	
+}
+.feed-list-item {
+	margin-top: 12px;
+	padding: 16px;
+	background-color: #ffffff;
+}
+.feed-list-header {
+	display: flex;
+	align-items: center;
+}
+.feed-list-avatar {
+	border-radius: 100px;
+	width: 35px;
+	height: 35px;
+}
+.feed-list-name {
+	flex: 1;
+	width: 0;
+	margin-left: 10px;
+	font-size: 15px;
+	color: $M08;
+}
+.feed-list-body {
+	padding-top: 10px;
+}
+.feed-list-content {
+	margin-bottom: 12px;
+	font-size: 16px;
+	color: #000000;
+	line-height: 26px;
+}
+.feed-list-tags {
+	margin-bottom: 12px;
+}
+.feed-list-address {
+	font-size: 12px;
+	color: $M08;
+}
+.feed-list-location-icon {
+	position: relative;
+	top: 1px;
+	margin-right: 5px;
+	width: 11px;
+	height: 11px;
+}
+</style>
--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,15 +1,80 @@
 <template>
   <c-page title="首页" :has-back="false">
-		<navigator url="/pages/test/index">========test========</navigator>
+		<feed-list :list="feedList" @item-click="handleFeedItemClick"></feed-list>
 	</c-page>
 </template>
 
 <script>
+	import FeedList from '@/library/components/feed-list'
+	
   export default {
+		components: {
+			FeedList
+		},
     data() {
       return {
+				feedList: [
+					{
+						avatar: 'https://mmbiz.qpic.cn/mmbiz_gif/1qRdrtrbict1lYcqGYFpzS7Y4zYiar4nXeKYNNb34j4SYfhAzoEs1yp1VcDLYKiaYJicNgHs4wyEGX8632Ms2Z7Jxg/640?wx_fmt=gif&tp=webp&wxfrom=5&wx_lazy=1',
+						name: 'zhang先生',
+						address: '广东省佛山市顺德区',
+						content: '肯德基疯狂星期四”活动时间为6月20日、6月27日。活动日的9:30-23:00（北京市、天津市、南京市、无锡市、湖北省、江西省、广东省、福建省、广西壮族自治区和海南省等部分地区仅限10:30－23:00供应），具体以餐厅实际营业时间为准，到店可享以9.9元优惠价购买薯条（小）2份，以19.9元优惠价购买老北京香辣鸭肉卷2个，以及以19.9元优惠价购买桃之恋乌龙茶2杯。',
+						petImages: [
+							'https://mmbiz.qpic.cn/mmbiz_gif/1qRdrtrbict1lYcqGYFpzS7Y4zYiar4nXeKYNNb34j4SYfhAzoEs1yp1VcDLYKiaYJicNgHs4wyEGX8632Ms2Z7Jxg/640?wx_fmt=gif&tp=webp&wxfrom=5&wx_lazy=1',
+							'https://mmbiz.qpic.cn/mmbiz_gif/1qRdrtrbict1lYcqGYFpzS7Y4zYiar4nXeKYNNb34j4SYfhAzoEs1yp1VcDLYKiaYJicNgHs4wyEGX8632Ms2Z7Jxg/640?wx_fmt=gif&tp=webp&wxfrom=5&wx_lazy=1',
+							'https://mmbiz.qpic.cn/mmbiz_gif/1qRdrtrbict1lYcqGYFpzS7Y4zYiar4nXeKYNNb34j4SYfhAzoEs1yp1VcDLYKiaYJicNgHs4wyEGX8632Ms2Z7Jxg/640?wx_fmt=gif&tp=webp&wxfrom=5&wx_lazy=1',
+							'https://mmbiz.qpic.cn/mmbiz_gif/1qRdrtrbict1lYcqGYFpzS7Y4zYiar4nXeKYNNb34j4SYfhAzoEs1yp1VcDLYKiaYJicNgHs4wyEGX8632Ms2Z7Jxg/640?wx_fmt=gif&tp=webp&wxfrom=5&wx_lazy=1',
+							'https://mmbiz.qpic.cn/mmbiz_gif/1qRdrtrbict1lYcqGYFpzS7Y4zYiar4nXeKYNNb34j4SYfhAzoEs1yp1VcDLYKiaYJicNgHs4wyEGX8632Ms2Z7Jxg/640?wx_fmt=gif&tp=webp&wxfrom=5&wx_lazy=1',
+						]
+					},
+					{
+						avatar: 'https://mmbiz.qpic.cn/mmbiz_jpg/1qRdrtrbict1R7pMvLMDI1mLRmsWNh5MKiaibCboHLHQ6AD0gUNcsX9dy9M4YuMnY46b5ibL1SkPuiaHmterWI06q5A/640?wx_fmt=jpeg&tp=webp&wxfrom=5&wx_lazy=1&wx_co=1',
+						name: 'zhang先生',
+						address: '广东省佛山市顺德区',
+						content: '肯德基疯狂星期四”活动时间为6月20日、6月27日。活动日的9:30-23:00（北京市、天津市、南京市、无锡市、湖北省、江西省、广东省、福建省、广西壮族自治区和海南省等部分地区仅限10:30－23:00供应），具体以餐厅实际营业时间为准，到店可享以9.9元优惠价购买薯条（小）2份，以19.9元优惠价购买老北京香辣鸭肉卷2个，以及以19.9元优惠价购买桃之恋乌龙茶2杯。',
+						petImages: [
+							'https://mmbiz.qpic.cn/mmbiz_jpg/1qRdrtrbict1R7pMvLMDI1mLRmsWNh5MKW1K3gselO4ohdf0GBMFEsLPbqXy9Cqe6ZkWl5AJctHHibtQLo7I4ibrw/640?wx_fmt=jpeg&tp=webp&wxfrom=5&wx_lazy=1&wx_co=1',
+							'https://mmbiz.qpic.cn/mmbiz_gif/1qRdrtrbict1lYcqGYFpzS7Y4zYiar4nXeKYNNb34j4SYfhAzoEs1yp1VcDLYKiaYJicNgHs4wyEGX8632Ms2Z7Jxg/640?wx_fmt=gif&tp=webp&wxfrom=5&wx_lazy=1',
+							'https://mmbiz.qpic.cn/mmbiz_jpg/1qRdrtrbict1R7pMvLMDI1mLRmsWNh5MKiaibCboHLHQ6AD0gUNcsX9dy9M4YuMnY46b5ibL1SkPuiaHmterWI06q5A/640?wx_fmt=jpeg&tp=webp&wxfrom=5&wx_lazy=1&wx_co=1'
+						]
+					},
+					{
+						avatar: 'https://mmbiz.qpic.cn/mmbiz_jpg/1qRdrtrbict1R7pMvLMDI1mLRmsWNh5MKiaibCboHLHQ6AD0gUNcsX9dy9M4YuMnY46b5ibL1SkPuiaHmterWI06q5A/640?wx_fmt=jpeg&tp=webp&wxfrom=5&wx_lazy=1&wx_co=1',
+						name: 'zhang先生',
+						address: '广东省佛山市顺德区',
+						content: '肯德基疯狂星期四”活动时间为6月20日、6月27日。活动日的9:30-23:00（北京市、天津市、南京市、无锡市、湖北省、江西省、广东省、福建省、广西壮族自治区和海南省等部分地区仅限10:30－23:00供应），具体以餐厅实际营业时间为准，到店可享以9.9元优惠价购买薯条（小）2份，以19.9元优惠价购买老北京香辣鸭肉卷2个，以及以19.9元优惠价购买桃之恋乌龙茶2杯。',
+						petImages: [
+							'https://mmbiz.qpic.cn/mmbiz_jpg/1qRdrtrbict1R7pMvLMDI1mLRmsWNh5MKW1K3gselO4ohdf0GBMFEsLPbqXy9Cqe6ZkWl5AJctHHibtQLo7I4ibrw/640?wx_fmt=jpeg&tp=webp&wxfrom=5&wx_lazy=1&wx_co=1',
+							'https://mmbiz.qpic.cn/mmbiz_gif/1qRdrtrbict1lYcqGYFpzS7Y4zYiar4nXeKYNNb34j4SYfhAzoEs1yp1VcDLYKiaYJicNgHs4wyEGX8632Ms2Z7Jxg/640?wx_fmt=gif&tp=webp&wxfrom=5&wx_lazy=1',
+							'https://mmbiz.qpic.cn/mmbiz_jpg/1qRdrtrbict1R7pMvLMDI1mLRmsWNh5MKiaibCboHLHQ6AD0gUNcsX9dy9M4YuMnY46b5ibL1SkPuiaHmterWI06q5A/640?wx_fmt=jpeg&tp=webp&wxfrom=5&wx_lazy=1&wx_co=1'
+						]
+					},
+					{
+						avatar: 'https://mmbiz.qpic.cn/mmbiz_jpg/1qRdrtrbict1R7pMvLMDI1mLRmsWNh5MKiaibCboHLHQ6AD0gUNcsX9dy9M4YuMnY46b5ibL1SkPuiaHmterWI06q5A/640?wx_fmt=jpeg&tp=webp&wxfrom=5&wx_lazy=1&wx_co=1',
+						name: 'zhang先生',
+						address: '广东省佛山市顺德区',
+						content: '肯德基疯狂星期四”活动时间为6月20日、6月27日。活动日的9:30-23:00（北京市、天津市、南京市、无锡市、湖北省、江西省、广东省、福建省、广西壮族自治区和海南省等部分地区仅限10:30－23:00供应），具体以餐厅实际营业时间为准，到店可享以9.9元优惠价购买薯条（小）2份，以19.9元优惠价购买老北京香辣鸭肉卷2个，以及以19.9元优惠价购买桃之恋乌龙茶2杯。',
+						petImages: [
+							'https://mmbiz.qpic.cn/mmbiz_jpg/1qRdrtrbict1R7pMvLMDI1mLRmsWNh5MKW1K3gselO4ohdf0GBMFEsLPbqXy9Cqe6ZkWl5AJctHHibtQLo7I4ibrw/640?wx_fmt=jpeg&tp=webp&wxfrom=5&wx_lazy=1&wx_co=1',
+							'https://mmbiz.qpic.cn/mmbiz_gif/1qRdrtrbict1lYcqGYFpzS7Y4zYiar4nXeKYNNb34j4SYfhAzoEs1yp1VcDLYKiaYJicNgHs4wyEGX8632Ms2Z7Jxg/640?wx_fmt=gif&tp=webp&wxfrom=5&wx_lazy=1',
+							'https://mmbiz.qpic.cn/mmbiz_gif/1qRdrtrbict1lYcqGYFpzS7Y4zYiar4nXeKYNNb34j4SYfhAzoEs1yp1VcDLYKiaYJicNgHs4wyEGX8632Ms2Z7Jxg/640?wx_fmt=gif&tp=webp&wxfrom=5&wx_lazy=1',
+							'https://mmbiz.qpic.cn/mmbiz_gif/1qRdrtrbict1lYcqGYFpzS7Y4zYiar4nXeKYNNb34j4SYfhAzoEs1yp1VcDLYKiaYJicNgHs4wyEGX8632Ms2Z7Jxg/640?wx_fmt=gif&tp=webp&wxfrom=5&wx_lazy=1',
+							'https://mmbiz.qpic.cn/mmbiz_gif/1qRdrtrbict1lYcqGYFpzS7Y4zYiar4nXeKYNNb34j4SYfhAzoEs1yp1VcDLYKiaYJicNgHs4wyEGX8632Ms2Z7Jxg/640?wx_fmt=gif&tp=webp&wxfrom=5&wx_lazy=1',
+							'https://mmbiz.qpic.cn/mmbiz_gif/1qRdrtrbict1lYcqGYFpzS7Y4zYiar4nXeKYNNb34j4SYfhAzoEs1yp1VcDLYKiaYJicNgHs4wyEGX8632Ms2Z7Jxg/640?wx_fmt=gif&tp=webp&wxfrom=5&wx_lazy=1',
+							'https://mmbiz.qpic.cn/mmbiz_jpg/1qRdrtrbict1R7pMvLMDI1mLRmsWNh5MKiaibCboHLHQ6AD0gUNcsX9dy9M4YuMnY46b5ibL1SkPuiaHmterWI06q5A/640?wx_fmt=jpeg&tp=webp&wxfrom=5&wx_lazy=1&wx_co=1'
+						]
+					}
+				]
       }
     },
+		methods: {
+			handleFeedItemClick (item) {
+				console.log(item, 'item')
+				uni.navigateTo({
+					url: '/pages/feed/detail/index'
+				})
+			} 
+		},
     onLoad() {
       this.$app.ready(() => {
 				this.$http.get('/pet/api/v1/pet/class').then(res => {
